aa64d45f2a0ab6709dece8a06eafdaa499800c3f
The-BlueBoy <aayyuush2006@gmail.com>
2026-09-19 15:46:00 +0530

    Create Home.aspx.cs

---
+++ Home.aspx.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace SmartHealthFitnessAssistant
+{
+    public partial class Home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // This page is a static landing page - no dynamic data to load.
+        }
+    }
+}
